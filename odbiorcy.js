d8c83dd6ff1d48aac5dd09077f3235f3cfacbed2
domino2137-cmd <jakub.paczek@lociechocinek.pl>
2026-07-31 19:36:06 +0200

    Update odbiorcy.js

--- odbiorcy.js
+++ odbiorcy.js
@@ -2,41 +2,13 @@
 
 window.PIRO_RECIPIENTS = {
   "63468235": {
-    "name": "Szymon Kędzierski",
+    "name": "-",
     "phone": "-",
     "email": "-",
     "deliveryAddress": "Sklep PiroPlanet (Odbiór Osobisty)",
     "items": [
-      { "name": "Petarda lontowa Explosive III EX3", "quantity": 1, "price": "4,00 zł" },
-      { "name": "Ps3 Shark Emiter Dźwięku 5 szt. ", "quantity": 1, "price": "30,00 zł" },
-      { "name": "Petardy Hukowe FP3 BIG Jorge – 20 sztuk ", "quantity": 1, "price": "17,00 zł" },
-      { "name": "FP6 Shark petardy JORGE 2 g", "quantity": 1, "price": "25,00 zł" },
-      { "name": "Wysyłka", "quantity": 1, "price": "6,00 zł" }
+      { "name": "-", "quantity": 1, "price": "0,00 zł" }
     ],
-    "totalValue": "82,00 zł"
+    "totalValue": "00,00 zł"
   },
-  "63468584": {
-    "name": "Michał Stasiak",
-    "phone": "+48 733 490 951",
-    "email": "-",
-    "deliveryAddress": "Sklep PiroPlanet (Odbiór Osobisty)",
-    "items": [
-      { "name": "Petarda lontowa FT4 Funke Trueno No. 4", "quantity": 1, "price": "69.50 zł" },
-      { "name": "Funke Petarda 3 Blue FB3B - 20 szt/op", "quantity": 2, "price": "79.00 zł" },
-      { "name": "PETARDA BŁYSKOWA Z CRAKLINGIEM H2 X-BULL", "quantity": 1, "price": "25.00 zł" }
-    ],
-    "totalValue": "173.50 zł"
-  },
-  "63468884": {
-    "name": "Jan Kowalski",
-    "phone": "+48 600 111 222",
-    "email": "j.kowalski@example.com",
-    "deliveryAddress": "Sklep Stacjonarny PiroPlanet (Odbiór Osobisty)",
-    "items": [
-      { "name": "Wyrzutnia Cobra 100 Strzałów 30mm", "quantity": 1, "price": "349.00 zł" },
-      { "name": "Rakiety Jupiter - Zestaw 6szt", "quantity": 2, "price": "89.00 zł" },
-      { "name": "Lont wolnopalny 10m", "quantity": 1, "price": "19.99 zł" }
-    ],
-    "totalValue": "546.99 zł"
-  }
 };
